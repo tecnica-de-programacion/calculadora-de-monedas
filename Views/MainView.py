c6d053709657ec6f89ff7539413704220482daf4
Miguelo Ruiz <red2000ace@gmail.com>
2017-10-25 07:40:47 -0500

    Added USD Label

--- Views/MainView.py
+++ Views/MainView.py
@@ -19,3 +19,18 @@ class MainView(Tk):
         self.title(self.Constants.title)
         self.maxsize(width=self.Constants.width, height=self.Constants.heigth)
         self.minsize(width=self.Constants.width, height=self.Constants.heigth)
+        self.__configure_grid()
+        self.__configure_UI()
+
+    def __configure_grid(self):
+        self.grid_rowconfigure(0, weight=True)
+        self.grid_rowconfigure(1, weight=True)
+        self.grid_rowconfigure(2, weight=True)
+        self.grid_columnconfigure(0, minsize=self.Constants.input_width)
+        self.grid_columnconfigure(2, minsize=self.Constants.input_width)
+        self.grid_columnconfigure(1, minsize=self.Constants.separator_width)
+
+    def __configure_UI(self):
+        currency_name_label = Label(self)
+        currency_name_label.configure(text = "USD")
+        currency_name_label.grid(row = 0, column = 0, sticky = self.Constants.left)
