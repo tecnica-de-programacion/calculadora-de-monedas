aea10a47a97fc4e7e6cd469878ea23b6ce937ac8
Miguelo Ruiz <red2000ace@gmail.com>
2017-10-25 07:34:03 -0500

    Added MainView

---
+++ Views/MainView.py
@@ -0,0 +1,21 @@
+from tkinter import Tk, Label, Entry, N, S, E, W
+
+class MainView(Tk):
+    class Constants:
+        title = "Cambio de Moneda"
+        heigth = 100
+        width = 550
+        input_width = 250
+        separator_width = 50
+        center = N + S + E + W
+        left = W
+        event = "<Button-1>"
+
+        convert_text = "Convertir"
+        separator_text = "▶"
+
+    def __init__(self):
+        super().__init__()
+        self.title(self.Constants.title)
+        self.maxsize(width=self.Constants.width, height=self.Constants.heigth)
+        self.minsize(width=self.Constants.width, height=self.Constants.heigth)
