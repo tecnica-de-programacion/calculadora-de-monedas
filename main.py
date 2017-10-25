cd0b80ecff3309337467160b103da7ba557f07c7
Miguelo Ruiz <red2000ace@gmail.com>
2017-10-25 08:18:08 -0500

    Added Full convertion

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from Views.MainView import MainView
+from Models.Currency import Currency
 
 class MainApp():
     def __init__(self):
         self.__master = MainView(convert_handler = self.__convert)
+        self.__currency = Currency("USD", 19.24)
 
     def run(self):
         self.__master.mainloop()
 
     def __convert(self, from_currency, to_currency, ammount):
-        print(from_currency, to_currency, ammount)
+        result = str(self.__currency.get_convertion(ammount))
+        self.__master.update_result(result)
 
 if __name__ == "__main__":
     app = MainApp()
