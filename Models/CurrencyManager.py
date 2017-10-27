656f801d32ce95259ee8813747e68795063ff641
Miguelo Ruiz <red2000ace@gmail.com>
2017-10-27 07:14:03 -0500

    Added Server resquest

---
+++ Models/CurrencyManager.py
@@ -0,0 +1,15 @@
+from urllib import request
+import json
+from Models.Currency import Currency
+
+class CurrencyManager():
+    class Constants:
+        base_url = "http://api.fixer.io/latest?base="
+
+    @classmethod
+    def get_currency(cls, currency_name):
+        with request.urlopen(cls.Constants.base_url + currency_name) as response:
+            data = response.read().decode()
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Views.MainView import MainView
 from Models.Currency import Currency
+from Models.CurrencyManager import CurrencyManager
 
 class MainApp():
     def __init__(self):
         self.__master = MainView(convert_handler = self.__convert)
-        self.__currency = Currency("USD", 19.24)
+        self.__currency = CurrencyManager.get_currency("USD")
 
     def run(self):
         self.__master.mainloop()
