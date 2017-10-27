b63a071ce1a0ce113ba612b38f95bd5e3c94e5fa
Miguelo Ruiz <red2000ace@gmail.com>
2017-10-27 07:51:19 -0500

    Removed hardcoded USD

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Models.CurrencyManager import CurrencyManager
 class MainApp():
     def __init__(self):
         self.__master = MainView(convert_handler = self.__convert)
-        self.__currency = CurrencyManager.get_currency("USD")
+        self.__currency = CurrencyManager.get_currency("EUR")
 
     def run(self):
         self.__master.mainloop()
