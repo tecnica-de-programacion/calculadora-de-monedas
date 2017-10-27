d1ae5e1a9e96de907579b148494f23f81628f554
rulsanz <raul.sanz.hedz@gmail.com>
2017-10-27 08:14:45 -0500

    LAST

--- Models/Currency.py
+++ Models/Currency.py
@@ -1,11 +1,17 @@
 class Currency:
-    def __init__(self, name, rate):
-        self.__name = name
-        self.__rate = rate
+    def __init__(self, json):
+        self.__name = json["base"]
+        self.__date = json["date"]
+        self.__rates = json["rates"]
 
     @property
     def name(self):
         return self.__name
 
-    def get_convertion(self, ammount):
-        return ammount * self.__rate
+    def get_convertion(self, currency, ammount):
+        convert_value = self.__rates.get(currency, None)
+        if convert_value is None:
+            return None
+        return ammount * convert_value
+    def __repr__(self):
+        return'{}, {}'.format(self.__name, self.__date)
--- Models/CurrencyManager.py
+++ Models/CurrencyManager.py
@@ -8,8 +8,10 @@ class CurrencyManager():
 
     @classmethod
     def get_currency(cls, currency_name):
-        with request.urlopen(cls.Constants.base_url + currency_name) as response:
-            data = response.read().decode()
-
-
-
+        try:
+            with request.urlopen(cls.Constants.base_url + "USD") as response:
+                data = response.read().decode()
+                json_data = json.loads(data)
+                return Currency(json_data)
+        except Exception as error:
+            return None
