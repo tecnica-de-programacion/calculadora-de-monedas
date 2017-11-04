9b41362a44ad07591930cf4b03b9d8f28669c61d
Miguel Navarrete <miguel.angel.navarrete.meza@gmail.com>
2017-11-04 14:37:29 -0600

    Updated gitignore and main files

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Button, Entry, N, S, E, W
+from tkinter import Tk, Label, Button, Entry, N, S, E, W, OptionMenu
 
 class MainView(Tk):
     class Constants:
@@ -18,18 +18,18 @@ class MainView(Tk):
         super().__init__()
         self.__convert_handler = convert_handler
         self.title(self.Constants.title)
-        self.maxsize(width=self.Constants.width, height=self.Constants.heigth)
-        self.minsize(width=self.Constants.width, height=self.Constants.heigth)
+        self.maxsize(width = self.Constants.width, height = self.Constants.heigth)
+        self.minsize(width = self.Constants.width, height = self.Constants.heigth)
         self.__configure_grid()
         self.__configure_UI()
 
     def __configure_grid(self):
-        self.grid_rowconfigure(0, weight=True)
-        self.grid_rowconfigure(1, weight=True)
-        self.grid_rowconfigure(2, weight=True)
-        self.grid_columnconfigure(0, minsize=self.Constants.input_width)
-        self.grid_columnconfigure(2, minsize=self.Constants.input_width)
-        self.grid_columnconfigure(1, minsize=self.Constants.separator_width)
+        self.grid_rowconfigure(0, weight = True)
+        self.grid_rowconfigure(1, weight = True)
+        self.grid_rowconfigure(2, weight = True)
+        self.grid_columnconfigure(0, minsize = self.Constants.input_width)
+        self.grid_columnconfigure(2, minsize = self.Constants.input_width)
+        self.grid_columnconfigure(1, minsize = self.Constants.separator_width)
 
     def __configure_UI(self):
         currency_name_label = Label(self)
@@ -38,24 +38,24 @@ class MainView(Tk):
 
         result_name_label = Label(self)
         result_name_label.configure(text="MXN")
-        result_name_label.grid(row=0, column=2, sticky=self.Constants.left)
+        result_name_label.grid(row = 0, column = 2, sticky = self.Constants.left)
 
         separator_label = Label(self)
-        separator_label.configure(text= self.Constants.separator_text)
-        separator_label.grid(row=1, column=1, sticky=self.Constants.center)
+        separator_label.configure(text = self.Constants.separator_text)
+        separator_label.grid(row = 1, column = 1, sticky = self.Constants.center)
 
         self.__result_label = Label(self)
         self.__result_label.configure(text="0")
-        self.__result_label.grid(row=1, column=2, sticky=self.Constants.left)
+        self.__result_label.grid(row = 1, column = 2, sticky = self.Constants.left)
 
         self.__convert_button = Button(self)
         self.__convert_button.configure(text = self.Constants.convert_text)
-        self.__convert_button.grid(row=2, column=2, sticky=self.Constants.center)
+        self.__convert_button.grid(row = 2, column = 2, sticky = self.Constants.center)
         self.__convert_button.bind(self.Constants.event, self.__did_tap_convert)
 
         vcmd = (self.register(self.__checkNumberOnly), '%d', '%P')
-        self.__currency_input = Entry(self, validate="key", validatecommand = vcmd)
-        self.__currency_input.grid(row=1, column=0, sticky=self.Constants.center)
+        self.__currency_input = Entry(self, validate = "key", validatecommand = vcmd)
+        self.__currency_input.grid(row = 1, column = 0, sticky = self.Constants.center)
 
     def __did_tap_convert(self, event):
         if self.__convert_handler is None:
@@ -65,11 +65,10 @@ class MainView(Tk):
         except ValueError:
             return
         else:
-            self.__convert_handler("USD", "MXN", ammount_to_convert)
+            self.__convert_handler("USD", "EUR", ammount_to_convert)
 
     def update_result(self, text):
-        self.__result_label.configure(text=text)
-
+        self.__result_label.configure(text = text)
 
     def __checkNumberOnly(self, action, value_if_allowed):
         if action != '1':
@@ -80,6 +79,3 @@ class MainView(Tk):
             return False
         else:
             return True
-
-
-
