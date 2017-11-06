e4f1ba092ae780637679f62d0f3539a92a36c07c
sergilgg <sergil51@hotmail.com>
2017-11-05 20:16:57 -0600

    Models JSON

--- Models/Currency.py
+++ Models/Currency.py
@@ -1,11 +1,15 @@
 class Currency:
-    def __init__(self, name, rate):
-        self.__name = name
-        self.__rate = rate
+    def __init__(self, __name, __rate):
+        self.__name = json["base"]
+        self.__date = json["date"]
+        self.__rates = json["rates"]
 
     @property
     def name(self):
         return self.__name
 
     def get_convertion(self, ammount):
-        return ammount * self.__rate
+        convert_value = self.__rates.get(currency, None)
+        if convert_value is None:
+            return None
+        return ammount * convert_value
