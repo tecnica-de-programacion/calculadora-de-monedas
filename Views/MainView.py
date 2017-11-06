305c3f643d75e88d836942f797c17862f0a6f6f7
sergilgg <sergil51@hotmail.com>
2017-11-05 20:56:31 -0600

    Menus Currency

--- Views/MainView.py
+++ Views/MainView.py
@@ -1,27 +1,31 @@
-from tkinter import Tk, Label, Button, Entry, N, S, E, W
+from tkinter import Tk, Label, Button, Entry, N, S, E, W, StringVar, OptionMenu
 
 class MainView(Tk):
     class Constants:
         title = "Cambio de Moneda"
-        heigth = 100
+        heigth = 250
         width = 550
-        input_width = 250
+        input_width = 200
         separator_width = 50
         center = N + S + E + W
         left = W
+        right = E
         event = "<Button-1>"
 
-        convert_text = "Convertir"
+        convert_text = "Convert"
         separator_text = "▶"
 
     def __init__(self, convert_handler = None):
         super().__init__()
+        self.origin_currency = 'EUR'
+        self.converted_currency = 'MXN'
         self.__convert_handler = convert_handler
         self.title(self.Constants.title)
         self.maxsize(width=self.Constants.width, height=self.Constants.heigth)
         self.minsize(width=self.Constants.width, height=self.Constants.heigth)
         self.__configure_grid()
         self.__configure_UI()
+
 
     def __configure_grid(self):
         self.grid_rowconfigure(0, weight=True)
@@ -32,13 +36,17 @@ class MainView(Tk):
         self.grid_columnconfigure(1, minsize=self.Constants.separator_width)
 
     def __configure_UI(self):
-        currency_name_label = Label(self)
-        currency_name_label.configure(text = "USD")
-        currency_name_label.grid(row = 0, column = 0, sticky = self.Constants.left)
 
-        result_name_label = Label(self)
-        result_name_label.configure(text="MXN")
-        result_name_label.grid(row=0, column=2, sticky=self.Constants.left)
+        self.__currencys = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY", "CZK", "DKK", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "RUB", "SEK", "SGD", "THB" ,"TRY" ,"ZAR", "EUR"]
+        self.origin = StringVar(self)
+        self.origin.set('Origin currency')
+        self.converted = StringVar(self)
+        self.converted.set('Converted currency')
+
+        self.menuO = OptionMenu(self, self.origin, self.__currencys)
+        self.menuO.grid(row=0, column=0, sticky=self.Constants.left)
+        self.menuC = OptionMenu(self, self.converted, self.__currencys)
+        self.menuC.grid(row=0, column=2, sticky=self.Constants.left)
 
         separator_label = Label(self)
         separator_label.configure(text= self.Constants.separator_text)
@@ -50,7 +58,7 @@ class MainView(Tk):
 
         self.__convert_button = Button(self)
         self.__convert_button.configure(text = self.Constants.convert_text)
-        self.__convert_button.grid(row=2, column=2, sticky=self.Constants.center)
+        self.__convert_button.grid(row=2, column=2, sticky=self.Constants.right)
         self.__convert_button.bind(self.Constants.event, self.__did_tap_convert)
 
         vcmd = (self.register(self.__checkNumberOnly), '%d', '%P')
